088cc752cbc034b80848f29dc6bb2b4df466bfbb
3521024 <3521024@ppti-14-408-11.ufr-info-p6.jussieu.fr>
2018-12-03 18:30:25 +0100

    Maj des données de base pr Files Bin

---
+++ src/partie3/ArbreBin.java
@@ -0,0 +1,20 @@
+package partie3;
+
+import java.util.ArrayList;
+
+import partie1.Cle;
+
+public class ArbreBin {
+
+	private Cle c;
+	private ArrayList<ArbreBin> sousarbres;
+	
+	public ArbreBin(Cle c) {
+		this.c = c;
+		sousarbres = new ArrayList<ArbreBin>();
+	}
+	
+	
+	
+	
+}
--- src/partie3/FileBinomiale.java
+++ src/partie3/FileBinomiale.java
@@ -1,5 +1,13 @@
 package partie3;
 
+import java.util.ArrayList;
+
 public class FileBinomiale {
 
+	ArrayList<ArbreBin> arbres;
+	public int size;
+	public FileBinomiale() {
+		arbres = new ArrayList<ArbreBin>();
+		size = 0;
+	}
 }
