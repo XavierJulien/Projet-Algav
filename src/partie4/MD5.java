4d6ece8cfe8d9ee45d5743ffa20238e891cb5264
XavierJulien <julienxavier774@gmail.com>
2018-12-18 01:14:05 +0100

    Fin MD5 + Ajuster es finittions + jeux de tests

--- src/partie4/MD5.java
+++ src/partie4/MD5.java
@@ -1,35 +1,127 @@
 package partie4;
 
-public class MD5
-{
 
-	private static final int INIT_A = 0x67452301;
-	private static final int INIT_B = (int)0xEFCDAB89L;
-	private static final int INIT_C = (int)0x98BADCFEL;
-	private static final int INIT_D = 0x10325476;
 
-	private static final int[] SHIFT_AMTS = {
-			7, 12, 17, 22,
-			5,  9, 14, 20,
-			4, 11, 16, 23,
-			6, 10, 15, 21
+public class MD5 {
+
+	private static final int a0 = 0x67452301;
+	private static final int b0 = (int)0xEFCDAB89;
+	private static final int c0= (int)0x98BADCFE;
+	private static final int d0 = 0x10325476;
+
+	private int R[] = {
+			7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22, 
+			5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20, 
+			4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23, 
+			6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21
 	};
 
-	private static final int[] TABLE_T = new int[64];
-	static
-	{
-		for (int i = 0; i < 64; i++)
-			TABLE_T[i] = (int)(long)((1L << 32) * Math.abs(Math.sin(i + 1)));
+	private int[] K = new int[64]; 
+
+
+	public String md5(byte[] message) {
+
+		//MD5 utilise des sinus d'entiers pour ses constantes :
+		for (int i = 0; i < 64; i++) K[i] = (int)(long)(Math.abs(Math.sin(i+1)) * Math.pow(2, 32));
+
+		//Préparation des variables :
+		int a = a0;
+		int b = b0;
+		int c = c0;
+		int d = d0;
+
+		//Préparation du message (padding) :
+		int messageLen = message.length;
+		int nb_block = ((messageLen + 8) >>> 6) + 1;
+		int totalLen = (int)(nb_block*Math.pow(2, 6));//taille du message initial en 64 bits
+		byte[] padded_mess = new byte[totalLen - messageLen];
+		padded_mess[0] = (byte)0x80;//ajout du bit "1" au message
+		int sizeBits = (int)(messageLen*Math.pow(2,3)); // taille du message en binaire ( 8-bit par lettre)
+		for (int i = 0; i < 8; i++)// on met la taille en little endian
+		{
+			padded_mess[padded_mess.length - 8 + i] = (byte)sizeBits;//premier est la taille 
+			sizeBits >>>= 8;//on descend sizebits de 8 shift à droite 
+		}
+		//Découpage en blocs de 512 bits :
+		for(int i = 0;i<nb_block;i++) {
+			int index = i << 6;
+			System.out.println(index);
+			int[] buffer = new int[16];
+			for(int j = 0;j<64;j++,index++) {
+				if(index < messageLen) {
+					//j >>> 2 est l'indice de 0 à 16 en unsigned
+					buffer[j >>> 2] = ((int)(message[index] << 24) | (buffer[j >>> 2] >>> 8));//
+				}else {
+					buffer[j >>> 2] = ((int)(padded_mess[index - message.length] << 24) | (buffer[j >>> 2] >>> 8));
+				}
+			}
+
+			//initialiser les valeurs de hachage :
+			int A = a;
+			int B = b;
+			int C = c;
+			int D = d;
+
+			//Boucle principale :
+			for(int k = 0; k < 64;k++) {
+				int F = 0;
+				int g = k;
+				if(k>=0 && k<=15) {
+					F = (B & C) | (~B & D);
+					g = k;
+				}
+				if(k>=16 && k<=31) {
+					F = (B & D) | (C & ~D);
+					g = (5*k + 1)%16 & 0x0F;
+				}
+				if(k>=32 && k<=47) {
+					F = B ^ C ^ D;
+					g = (3*k + 5)%16 & 0x0F;
+				}
+				if(k>=48 && k<=63) {
+					F = C ^ (B | ~D);
+					g = (7*k)%16 & 0x0F;
+				}
+				int temp = B + Integer.rotateLeft((A + F + K[k] + buffer[g]),R[k]);
+				A = D;
+				D = C;
+				C = B;
+				B = temp;
+			}
+			//ajouter le résultat au bloc précédent :
+			a += A;
+			b += B;
+			c += C;
+			d += D;
+		}
+
+		//var entier empreinte := h0 concaténer h1 concaténer h2 concaténer h3 //(en little-endian)
+		byte[] md5 = new byte[16];
+
+		int count = 0;
+		for (int i = 0; i < 4; i++)
+		{
+			int n = (i == 0) ? a : ((i == 1) ? b : ((i == 2) ? c : d));
+			for (int j = 0; j < 4; j++)
+			{
+				md5[count++] = (byte)n;
+				n >>>= 8;
+			}
+		}
+		return "0x"+toHexString(md5);
 	}
 
-	public static byte[] computeMD5(byte[] message)
+	public byte[] computeMD5(byte[] message)
 	{
+		for (int i = 0; i < 64; i++) K[i] = (int)(long)(Math.abs(Math.sin(i+1)) * Math.pow(2, 32));
+
 		int messageLenBytes = message.length;
 		int numBlocks = ((messageLenBytes + 8) >>> 6) + 1;
 		int totalLen = numBlocks << 6;
 		byte[] paddingBytes = new byte[totalLen - messageLenBytes];
 		paddingBytes[0] = (byte)0x80;
 
+		System.out.println(totalLen - messageLenBytes);
 		long messageLenBits = (long)messageLenBytes << 3;
 		for (int i = 0; i < 8; i++)
 		{
@@ -37,16 +129,18 @@ public class MD5
 			messageLenBits >>>= 8;
 		}
 
-		int a = INIT_A;
-		int b = INIT_B;
-		int c = INIT_C;
-		int d = INIT_D;
+		int a = a0;
+		int b = b0;
+		int c = c0;
+		int d = d0;
 		int[] buffer = new int[16];
 		for (int i = 0; i < numBlocks; i ++)
 		{
 			int index = i << 6;
 			for (int j = 0; j < 64; j++, index++)
-				buffer[j >>> 2] = ((int)((index < messageLenBytes) ? message[index] : paddingBytes[index - messageLenBytes]) << 24) | (buffer[j >>> 2] >>> 8);
+			{
+				buffer[j >>> 2] = ((int) ((index < messageLenBytes) ? message[index] : paddingBytes[index - messageLenBytes]) << 24) | (buffer[j >>> 2] >>> 8);
+			}
 			int originalA = a;
 			int originalB = b;
 			int originalC = c;
@@ -77,7 +171,8 @@ public class MD5
 					bufferIndex = (bufferIndex * 7) & 0x0F;
 					break;
 				}
-				int temp = b + Integer.rotateLeft(a + f + buffer[bufferIndex] + TABLE_T[j], SHIFT_AMTS[(div16 << 2) | (j & 3)]);
+				//System.out.println(bufferIndex);
+				int temp = b + Integer.rotateLeft(a + f + buffer[bufferIndex] + K[j], R[j]);
 				a = d;
 				d = c;
 				c = b;
@@ -88,12 +183,10 @@ public class MD5
 			b += originalB;
 			c += originalC;
 			d += originalD;
-			System.out.println(a);
+			/*System.out.println(a);
 			System.out.println(b);
 			System.out.println(c);
-			System.out.println(d);
-			
-			
+			System.out.println(d);*/
 		}
 
 		byte[] md5 = new byte[16];
@@ -109,8 +202,7 @@ public class MD5
 		}
 		return md5;
 	}
-
-	public static String toHexString(byte[] b)
+	public String toHexString(byte[] b)
 	{
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < b.length; i++)
@@ -120,12 +212,66 @@ public class MD5
 		return sb.toString();
 	}
 
-	public static void main(String[] args)
-	{
-		String[] testStrings = { "", "a", "abc", "message digest", "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", "12345678901234567890123456789012345678901234567890123456789012345678901234567890" };
-		for (String s : testStrings)
-			System.out.println("0x" + toHexString(computeMD5(s.getBytes())) + " <== \"" + s + "\"");
-		return;
+
+
+	//0xabcdefgh => 0xghefcdab
+	public String padding(String mess) {//Pre-processing: padding with zeros
+		byte[]mess_byte = mess.getBytes();
+		String res = "";
+		String resHexa = "";
+		/*****Mot en Hexa*****/
+		for(int i = 0;i<mess_byte.length;i++) {
+			String temp = Integer.toBinaryString(mess_byte[i]);
+			while(temp.length() < 8 ) {
+				temp = "0"+temp;
+			}
+			res += temp;
+		}
+		String sizebin = Integer.toBinaryString(res.length());
+		/*****Ajout du 1*****/
+		res = res + "1";
+		while(res.length()%512 != 448) {
+			res = res+"0";
+		}
+		resHexa = toHexString(res);
+		/*****SIZE*****/
+		while(sizebin.length()<8) {
+			sizebin = "0"+sizebin;
+		}
+		while(sizebin.length()<64) {
+			sizebin = sizebin+"0";
+		}
+		String sizeHex = toHexString(sizebin);
+		return resHexa+sizeHex;
 	}
 
+	public String toHexString(Integer s){return Integer.toString(s,16);}
+
+	public String toHexString(String s) {
+		String res = "";
+		if(s.length()%8 == 0) {
+			for(int i = 0;i<s.length();i = i+4) {
+				String temp = s.substring(i, i+4);
+				int decimal = Integer.parseInt(temp,2);
+				res = res + Integer.toString(decimal,16);
+			}
+		}
+		return res;
+	}
+
+	public byte[] messPad(byte[] message) {
+		int messageLenBytes = message.length;
+		int numBlocks = ((messageLenBytes + 8) >>> 6) + 1;
+		int totalLen = numBlocks << 6;
+		byte[] paddingBytes = new byte[totalLen - messageLenBytes];
+		paddingBytes[0] = (byte)0x80;
+
+		long messageLenBits = (long)messageLenBytes << 3;
+		for (int i = 0; i < 8; i++)
+		{
+			paddingBytes[paddingBytes.length - 8 + i] = (byte)messageLenBits;
+			messageLenBits >>>= 8;
+		}
+		return paddingBytes;
+	}
 }
--- src/partie4/TestMD5.java
+++ src/partie4/TestMD5.java
@@ -3,18 +3,9 @@ package partie4;
 public class TestMD5{
 
 	public static void main(String[] args){
-		MD5t m = new MD5t();
-		System.out.println(m.md5("Wikipedia est pas mal comme navigateur de feffffffffffffffffffffrecherche"));
-		System.out.println(Integer.toBinaryString(Integer.parseInt("61", 16)));
-		/*for(int i = 0 ; i<res.length;i++)
-			System.out.println(res[i]);*/
-		//System.out.println("Résultat attendu : ");
-		/*System.out.println("-645128748\r\n" + 
-				"78774415\r\n" + 
-				"-1744207639\r\n" + 
-				"2118318316");*/
-		//System.out.println("0xD41D8CD98F00B204E9800998ECF8427E <== \"\"");
-		//byte[] b = new MD5t().messPad("".getBytes());
+		MD5 m = new MD5();
+		//System.out.println("0x"+m.toHexString(m.computeMD5("a".getBytes())));
+		System.out.println(m.md5("a".getBytes()));
 	}
 
 }
